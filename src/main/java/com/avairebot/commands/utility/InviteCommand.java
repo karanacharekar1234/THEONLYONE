62f3ba25ab316806c9f303db34990c31a876aaff
karanacharekar1234 <50902982+karanacharekar1234@users.noreply.github.com>
2020-11-12 01:26:43 +0530

    Update InviteCommand.java

--- src/main/java/com/avairebot/commands/utility/InviteCommand.java
+++ src/main/java/com/avairebot/commands/utility/InviteCommand.java
@@ -51,7 +51,7 @@ public class InviteCommand extends Command {
 
     @Override
     public List<String> getUsageInstructions() {
-        return Collections.singletonList("`:command` - Gives you an invite link that can be used to invite AvaIre to servers.");
+        return Collections.singletonList("`:command` - Gives you an invite link that can be used to invite BETHEONE to servers.");
     }
 
     @Override
